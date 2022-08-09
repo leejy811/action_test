50ee714e552c4718dbbeb48ca0939b04b068a9ef
leejy811 <lee38956162@gmail.com>
2022-08-09 22:16:52 +0900

    점프 & 회피 구현
    + 벽과 플레이어 충돌 시 지속적으로 한 방향으로 이동하면 플레이어가 벽을 뚫고 지나가는 문제 발견 고치려 노력해봤지만 이는 후에 다른 강의에서 고칠 예정이므로 그때 버그 수정할 것

--- action_test/Assets/Script/Player.cs
+++ action_test/Assets/Script/Player.cs
@@ -6,32 +6,105 @@ public class Player : MonoBehaviour
 {
     public float speed;
     public float ratioOfRunWalk;
+    public float jumpPower;
 
     float hAxis;
     float vAxis;
     bool wDown;
+    bool jDown;
+
+    bool isDodge;
 
     Vector3 moveVec;
+    Vector3 dodgeVec;
 
-    Animator animator;
+    Rigidbody rigid;
+    Animator anim;
     void Start()
     {
-        animator = GetComponentInChildren<Animator>();
+        rigid = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
     }
 
-    void Update()
+    void FixedUpdate()
+    {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+        Dodge();
+    }
+
+    void GetInput()
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
+        jDown = Input.GetButton("Jump");
+    }
 
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        if (isDodge)
+            moveVec = dodgeVec;
 
         transform.position += moveVec * speed * (wDown ? ratioOfRunWalk : 1f) * Time.deltaTime;
 
-        animator.SetBool("isRun", moveVec != Vector3.zero);
-        animator.SetBool("isWalk", wDown);
+        MoveAnim();
+    }
 
+    void MoveAnim()
+    {
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("isWalk", wDown);
+    }
+
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if (jDown && !IsJump() && !isDodge && moveVec == Vector3.zero)
+        {
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+        }
+    }
+
+    bool IsJump()
+    {
+        return anim.GetBool("isJump");
+    }
+
+    void Dodge()
+    {
+        if (jDown && !IsJump() && !isDodge && moveVec != Vector3.zero)
+        {
+            dodgeVec = moveVec;
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.4f);
+        }
+    }
+
+    void DodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+        }
     }
 }
