35be1a5510d9f152b947f5698bbc989d76b0c6b2
leejy811 <lee38956162@gmail.com>
2022-12-06 02:34:26 +0900

    보스 구현

--- action_test/Assets/Script/Enemy.cs
+++ action_test/Assets/Script/Enemy.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public enum Type { A, B, C };
+    public enum Type { A, B, C, D };
 
     public Type enemyType;
     public int maxHealth;
@@ -16,22 +16,24 @@ public class Enemy : MonoBehaviour
     public GameObject bullet;
     public bool isChase;
     public bool isAttack;
+    public bool isDead;
 
-    Rigidbody rigid;
-    BoxCollider boxCollider;
-    Material mat;
-    NavMeshAgent nav;
-    Animator anim;
+    public Rigidbody rigid;
+    public BoxCollider boxCollider;
+    public MeshRenderer[] meshs;
+    public NavMeshAgent nav;
+    public Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
         boxCollider = GetComponent<BoxCollider>();
-        mat = GetComponentInChildren<MeshRenderer>().material;
+        meshs = GetComponentsInChildren<MeshRenderer>();
         anim = GetComponentInChildren<Animator>();
         nav = GetComponent<NavMeshAgent>();
-
-        Invoke("ChaseStart", 2);
+        
+        if (enemyType != Type.D)
+            Invoke("ChaseStart", 2);
     }
 
     void ChaseStart()
@@ -42,7 +44,7 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
-        if (nav.enabled)
+        if (nav.enabled && enemyType != Type.D)
         {
             nav.SetDestination(target.position);
             nav.isStopped = !isChase;
@@ -57,30 +59,33 @@ public class Enemy : MonoBehaviour
 
     void Targeting()
     {
-        float targetRadius = 0;
-        float targetRange = 0;
-
-        switch (enemyType)
+        if (!isDead && enemyType != Type.D)
         {
-            case Type.A:
-                targetRadius = 1.5f;
-                targetRange = 3f;
-                break;
-            case Type.B:
-                targetRadius = 1f;
-                targetRange = 12f;
-                break;
-            case Type.C:
-                targetRadius = 0.5f;
-                targetRange = 25f;
-                break;
-        }
+            float targetRadius = 0;
+            float targetRange = 0;
 
-        RaycastHit[] raycastHits = Physics.SphereCastAll(transform.position, targetRadius, transform.forward, targetRange, LayerMask.GetMask("Player"));
+            switch (enemyType)
+            {
+                case Type.A:
+                    targetRadius = 1.5f;
+                    targetRange = 3f;
+                    break;
+                case Type.B:
+                    targetRadius = 1f;
+                    targetRange = 12f;
+                    break;
+                case Type.C:
+                    targetRadius = 0.5f;
+                    targetRange = 25f;
+                    break;
+            }
 
-        if(raycastHits.Length > 0 && !isAttack)
-        {
-            StartCoroutine(Attack());
+            RaycastHit[] raycastHits = Physics.SphereCastAll(transform.position, targetRadius, transform.forward, targetRange, LayerMask.GetMask("Player"));
+
+            if (raycastHits.Length > 0 && !isAttack)
+            {
+                StartCoroutine(Attack());
+            }
         }
     }
 
@@ -111,6 +116,7 @@ public class Enemy : MonoBehaviour
                 meleeArea.enabled = false;
 
                 yield return new WaitForSeconds(2f);
+
                 break;
             case Type.C:
                 yield return new WaitForSeconds(0.5f);
@@ -167,18 +173,24 @@ public class Enemy : MonoBehaviour
 
     IEnumerator OnDamage(Vector3 reactVec, bool isGrenade)
     {
-        mat.color = Color.red;
+        foreach (MeshRenderer mesh in meshs)
+            mesh.material.color = Color.red;
+
         yield return new WaitForSeconds(0.1f);
 
         if(curHealth > 0)
         {
-            mat.color = Color.white;
+            foreach (MeshRenderer mesh in meshs)
+                mesh.material.color = Color.white;
         }
         else
         {
-            mat.color = Color.gray;
+            foreach (MeshRenderer mesh in meshs)
+                mesh.material.color = Color.gray;
+
             gameObject.layer = 12;
 
+            isDead = true;
             isChase = false;
             nav.enabled = false;
             anim.SetTrigger("doDie");
@@ -189,13 +201,14 @@ public class Enemy : MonoBehaviour
                 float explosionPower = (reactVec.magnitude - 20) * -0.5f;
                 rigid.AddForce((reactVec.normalized + Vector3.up * 3) * explosionPower, ForceMode.Impulse);
                 rigid.AddTorque((reactVec.normalized + Vector3.up * 3) * explosionPower * 3f, ForceMode.Impulse);
-                Destroy(gameObject, 4f);
             }
             else  
             {
                 rigid.AddForce((reactVec.normalized + Vector3.up) * 5, ForceMode.Impulse);
-                Destroy(gameObject, 4f);
             }
+
+            if (enemyType != Type.D)
+                Destroy(gameObject, 4f);
         }
     }
 }
--- action_test/Assets/Script/Player.cs
+++ action_test/Assets/Script/Player.cs
@@ -327,22 +327,27 @@ public class Player : MonoBehaviour
                 Bullet enemyBullet = other.GetComponent<Bullet>();
                 health -= enemyBullet.damage;
 
-                if (other.GetComponent<Rigidbody>() != null)
-                {
-                    Destroy(other.gameObject);
-                }
-                StartCoroutine(OnDamage());
-            }      
+                bool isBossAttack = other.name == "Boss Melee Area";
+                StartCoroutine(OnDamage(isBossAttack));
+            }
+
+            if (other.GetComponent<Rigidbody>() != null)
+            {
+                Destroy(other.gameObject);
+            }
         }
     }
 
-    IEnumerator OnDamage()
+    IEnumerator OnDamage(bool isBossAttack)
     {
         isDamage = true;
         foreach(MeshRenderer mesh in meshs)
         {
             mesh.material.color = Color.yellow;
         }
+
+        if (isBossAttack)
+            rigid.AddForce(transform.forward * -25, ForceMode.Impulse);
         yield return new WaitForSeconds(1f);
 
         isDamage = false;
@@ -350,6 +355,9 @@ public class Player : MonoBehaviour
         {
             mesh.material.color = Color.white;
         }
+
+        if (isBossAttack)
+            rigid.velocity = Vector3.zero;
     }
 
     void AssignItem(ref int hasItem, int maxHasItem, Item item)
